aba3e275a683c6e9a4eba3ff335c11d34225b652
Mateus Nhoato <manhoato@gmail.com>
2023-01-09 10:43:12 -0300

    Adding ViewModels and _LanchesResumo (partial view)

--- LanchesMac/Controllers/LancheController.cs
+++ LanchesMac/Controllers/LancheController.cs
@@ -1,4 +1,5 @@
-﻿using LanchesMac.Repositories.Interfaces;
+﻿using LanchesMac.ViewModels;
+using LanchesMac.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Controllers
@@ -14,8 +15,12 @@ namespace LanchesMac.Controllers
 
         public IActionResult List()
         {
-            var lanches = _lancheRepository.Lanches;
-            return View(lanches);
+            var lanchesListViewModel = new LancheListViewModel();
+            lanchesListViewModel.Lanches = _lancheRepository.Lanches;
+            lanchesListViewModel.CategoriaAtual = "Categoria Atual";
+         
+            return View(lanchesListViewModel);
+            
         }
     }
 }
