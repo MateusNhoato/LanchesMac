402349742615eb84d1274de861e00efe7f28f595
Mateus Nhoato <manhoato@gmail.com>
2023-01-12 10:43:09 -0300

    Adding ContatoController and Implementing search by category in LancheController

--- LanchesMac/Controllers/LancheController.cs
+++ LanchesMac/Controllers/LancheController.cs
@@ -1,6 +1,7 @@
 ﻿using LanchesMac.ViewModels;
 using LanchesMac.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using LanchesMac.Models;
 
 namespace LanchesMac.Controllers
 {
@@ -13,12 +14,40 @@ namespace LanchesMac.Controllers
             _lancheRepository = lancheRepository;
         }
 
-        public IActionResult List()
+        public IActionResult List(string categoria)
         {
-            var lanchesListViewModel = new LancheListViewModel();
-            lanchesListViewModel.Lanches = _lancheRepository.Lanches;
-            lanchesListViewModel.CategoriaAtual = "Categoria Atual";
-         
+            IEnumerable<Lanche> lanches;
+            string categoriaAtual = categoria;
+
+            if(string.IsNullOrEmpty(categoria))
+            {
+                lanches = _lancheRepository.Lanches.OrderBy(l => l.LancheId);
+                categoriaAtual = "Todos os lanches";
+            }
+            else if (string.Equals("Normal", categoria, StringComparison.OrdinalIgnoreCase))
+            {
+                lanches = _lancheRepository.Lanches
+                    .Where(l => l.Categoria.CategoriaNome.Equals("Normal"))
+                    .OrderBy(l => l.Nome);
+            }
+            else if(string.Equals("Natural", categoria, StringComparison.OrdinalIgnoreCase))
+            {
+                lanches = _lancheRepository.Lanches
+                        .Where(l => l.Categoria.CategoriaNome.Equals("Natural"))
+                        .OrderBy(l => l.Nome);
+            }
+            else
+            {
+                lanches = new List<Lanche>();
+                categoriaAtual = $"Categoria '{categoria}' não encontrada.";
+            }
+                  
+            var lanchesListViewModel = new LancheListViewModel
+            {
+                Lanches = lanches,
+                CategoriaAtual = categoriaAtual
+            };
+                    
             return View(lanchesListViewModel);
             
         }
