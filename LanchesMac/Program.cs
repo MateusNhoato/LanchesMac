44507fecac29a4ce6de2ff128b8e51fc4d086510
Mateus Nhoato <manhoato@gmail.com>
2023-01-27 14:04:12 -0300

    Removing Startup.cs. Adding AdminLanchesReport and README

--- LanchesMac/Program.cs
+++ LanchesMac/Program.cs
@@ -1,17 +1,131 @@
-namespace LanchesMac;
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args)
-           .Build()
-           .Run();
-    }
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+
+
+using LanchesMac.Areas.Admin.Servicos;
+using LanchesMac.Context;
+using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using LanchesMac.Repositories;
+using LanchesMac.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+using ReflectionIT.Mvc.Paging;
+using FastReport.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddSession();
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddMemoryCache();
+
+FastReport.Utils.RegisteredObjects.AddConnection(typeof(MsSqlDataConnection));
+
+builder.Services.Configure<ConfigurationImagens>(builder.Configuration.GetSection("ConfigurationPastaImagens"));
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // configuração de senha fraca, para teste
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 3;
+    options.Password.RequiredUniqueChars = 1;
+});
+
+
+builder.Services.AddTransient<ILancheRepository, LancheRepository>();
+builder.Services.AddTransient<ICategoriaRepository, CategoriaRepository>();
+builder.Services.AddTransient<IPedidoRepository, PedidoRepository>();
+builder.Services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
+builder.Services.AddScoped<RelatorioVendasService>();
+builder.Services.AddScoped<GraficoVendasService>();
+builder.Services.AddScoped<RelatoriosLanchesService>();
+builder.Services.AddScoped(sp => CarrinhoCompra.GetCarrinho(sp));
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Admin",
+        politica =>
+        {
+            politica.RequireRole("Admin");
+        });
+});
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddPaging(options =>
+{
+    options.ViewName = "Bootstrap4";
+    options.PageParameterName = "pageindex";
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseSession();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseFastReport();
+
+app.UseRouting();
+
+
+CriarPerfisUsuario(app);
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+
+app.UseEndpoints(endpoints =>
+{
+
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}");
+
+
+    endpoints.MapControllerRoute(
+        name: "categoriaFiltro",
+        pattern: "Lanche/{action}/{categoria?}",
+        defaults: new { Controller = "Lanche", action = "list" });
+
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
+
+app.Run();
+
+void CriarPerfisUsuario(WebApplication app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (var scope = scopedFactory.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<ISeedUserRoleInitial>();
+        service.SeedUsers();
+        service.SeedRoles();
+    }
 }
---
+++ README.md
@@ -0,0 +1,38 @@
+# Lanches Mac 🍔🍟
+Projeto feito durante o curso "ASP .NET Core MVC - Criando um Site do Zero" do professor Macoratti.
+
+## Tabela de Conteúdo
+- [Projeto](#lanches-mac--)
+- [Descrição](#descrição)
+- [Demonstração](#demonstração)
+- [Tecnologias](#tecnologias)
+- [Features](#features)
+    - [Área de Administração](#área-de-administração)
+
+## Demonstração 
+![Demo](https://media.giphy.com/media/UypNDlLLGDOMvhPhat/giphy.gif)
+## Tecnologias
+[![MVC](https://img.shields.io/badge/FrameWork-ASP%20.%20NET%20Core%20MVC-orange)](https://learn.microsoft.com/pt-br/aspnet/core/mvc/overview?view=aspnetcore-7.0)
+[![Linguagem](https://img.shields.io/badge/linguagem-C%23-brightgreen.svg)](https://docs.microsoft.com/pt-br/dotnet/csharp/)
+[![Banco](https://img.shields.io/badge/Banco%20de%20Dados-SQL-blue)](https://learn.microsoft.com/en-us/sql/?view=sql-server-ver16)
+[![Bootstrap](https://img.shields.io/badge/-Bootstrap-blue)](https://getbootstrap.com/docs/5.3/getting-started/introduction/)
+
+O projeto foi feito em C# no ASP .NET Core MVC, utilizando o Entity Framework Core, além de recursos do Bootstrap 5. Como banco de dados foi utilizado o SQL Server.
+
+GoogleChart e o FastReport também foram utilizados para análises na área de administração do site.
+
+## Descrição 
+O projeto foi feito para aprender e praticar as tecnologias relacionadas com criação de web applications no ASP .NET Core, utilizando o padrão MVC. Se trata de um site para vendas de lanches totalmente funcional e com área de administração que permite cadastro de novos produtos, gerenciamento dos pedidos, relatórios, gráficos de vendas, além de outros artifícios.
+
+## Features 
+- Sistema de cadastro de usuários
+- Sistema de pedidos
+- Área de Contato
+
+### Área de Administração 
+- Cadastro de novas categorias de produtos
+- Cadastro de novos produtos
+- Ver relatórios das vendas
+- Upload e remoção de imagens do banco de dados
+- Histórico de pedidos
+- Relatório das Categorias e Produtos
